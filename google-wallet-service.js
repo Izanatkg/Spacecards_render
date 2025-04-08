9e0a1828e209ccfd81d0d03010db03b1b19dd5aa
izanatkg <ulisesfargot@gmail.com>
2025-04-07 20:36:13 -0600

    feat: Cambiar logo y heroImage de Google Wallet por QR code dinámico del cliente

--- google-wallet-service.js
+++ google-wallet-service.js
@@ -188,12 +188,12 @@ class GoogleWalletService {
                 hexBackgroundColor: '#FF5733',
                 logo: {
                     sourceUri: {
-                        uri: 'https://i.imgur.com/FpqHJGe.png'
+                        uri: `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${id}`
                     }
                 },
                 heroImage: {
                     sourceUri: {
-                        uri: 'https://i.imgur.com/FpqHJGe.png'
+                        uri: `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${id}`
                     }
                 }
             };
